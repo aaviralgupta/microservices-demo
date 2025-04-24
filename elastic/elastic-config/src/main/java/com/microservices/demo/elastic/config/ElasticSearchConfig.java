8a57b8f74dda8e3565d523442c5f72b8497830da
aviral <aaviralgupta@gmail.com>
2025-04-24 16:58:01 +0530

    consuming messages from kafka and storing in elastic final changes

--- elastic/elastic-config/src/main/java/com/microservices/demo/elastic/config/ElasticSearchConfig.java
+++ elastic/elastic-config/src/main/java/com/microservices/demo/elastic/config/ElasticSearchConfig.java
@@ -1,10 +1,19 @@
 package com.microservices.demo.elastic.config;
 
 import com.microservices.demo.config.ElasticConfigData;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+
+import java.util.Objects;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.microservices.demo.elastic")
