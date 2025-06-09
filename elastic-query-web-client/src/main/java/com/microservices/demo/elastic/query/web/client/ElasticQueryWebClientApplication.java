bbea70fc5bc991d2c69a1d328e67c759f6d6fd6e
aviral <aaviralgupta@gmail.com>
2025-06-09 23:49:40 +0530

    created elastic-query-web-client module

---
+++ elastic-query-web-client/src/main/java/com/microservices/demo/elastic/query/web/client/ElasticQueryWebClientApplication.java
@@ -0,0 +1,14 @@
+package com.microservices.demo.elastic.query.web.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = "com.microservices.demo")
+@SpringBootApplication
+public class ElasticQueryWebClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticQueryWebClientApplication.class);
+    }
+
+}
